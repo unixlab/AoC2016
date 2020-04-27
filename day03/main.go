11565091832af164be30f84860daee70aaf99234
Felix Hillingshaeuser <felix@mxcore.de>
2020-04-27 21:00:56 +0200

    add day03 part 2

--- day03/main.go
+++ day03/main.go
@@ -14,6 +14,10 @@ func main() {
 
 	counter := 0
 
+	var row1 []int
+	var row2 []int
+	var row3 []int
+
 	for scanner.Scan() {
 		var sides [3]int
 		re := regexp.MustCompile(`([^ \t]+)`)
@@ -21,6 +25,23 @@ func main() {
 			sides[k], _ = strconv.Atoi(v)
 		}
 		if sides[0]+sides[1] > sides[2] && sides[0]+sides[2] > sides[1] && sides[1]+sides[2] > sides[0] {
+			counter++
+		}
+		row1 = append(row1, sides[0])
+		row2 = append(row2, sides[1])
+		row3 = append(row3, sides[2])
+	}
+	fmt.Println(counter)
+
+	counter = 0
+
+	var rows []int
+	rows = append(rows, row1...)
+	rows = append(rows, row2...)
+	rows = append(rows, row3...)
+
+	for i := 2; i < len(rows); i = i + 3 {
+		if rows[i-2]+rows[i-1] > rows[i] && rows[i-2]+rows[i] > rows[i-1] && rows[i-1]+rows[i] > rows[i-2] {
 			counter++
 		}
 	}
