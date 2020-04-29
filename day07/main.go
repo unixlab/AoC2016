2b25141305fc2fd70fe2c0ba9e936dd1c1fc2e5e
Felix Hillingshaeuser <felix@mxcore.de>
2020-04-29 21:58:18 +0200

    add day07 part 2

--- day07/main.go
+++ day07/main.go
@@ -19,6 +19,16 @@ func checkABBA(input string) bool {
 	return false
 }
 
+func getAbas(input string) []string {
+	var abas []string
+	for i := 1; i < len(input)-1; i++ {
+		if input[i-1] == input[i+1] && input[i] != input[i+1] {
+			abas = append(abas, input[i-1:i+2])
+		}
+	}
+	return abas
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
@@ -41,6 +51,36 @@ func main() {
 			if checkABBA(v) {
 				counter++
 				break
+			}
+		}
+	}
+	fmt.Println(counter)
+
+	file.Seek(0, 0)
+	scanner = bufio.NewScanner(file)
+	counter = 0
+LINES:
+	for scanner.Scan() {
+		var hyperNets []string
+		line := scanner.Text()
+		lineSupernet := line
+		for strings.Index(lineSupernet, "[") > -1 {
+			startSN := strings.Index(lineSupernet, "[")
+			endSN := strings.Index(lineSupernet, "]")
+			hyperNets = append(hyperNets, lineSupernet[startSN+1:endSN])
+			lineSupernet = lineSupernet[:startSN] + "|" + lineSupernet[endSN+1:]
+		}
+		var abas []string
+		for _, v := range strings.Split(lineSupernet, "|") {
+			abas = append(abas, getAbas(v)...)
+		}
+		for _, v := range abas {
+			reverseAba := v[1:2] + v[0:1] + v[1:2]
+			for _, net := range hyperNets {
+				if strings.Index(net, reverseAba) > -1 {
+					counter++
+					continue LINES
+				}
 			}
 		}
 	}
