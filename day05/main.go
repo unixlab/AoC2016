2846551534e1807ef56a4d3b9b4bed61e3752bbe
Felix Hillingshaeuser <felix@mxcore.de>
2020-04-29 17:50:26 +0200

    add day05 part 2

--- day05/main.go
+++ day05/main.go
@@ -4,22 +4,37 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	passwordPart2 := make([]string, 8)
 	input := "ffykfhsq"
-	found := 0
+	foundPart1 := 0
+	foundPart2 := 0
 	counter := 0
 
-	for found < 8 {
+	for foundPart1 < 8 || foundPart2 < 8 {
 		hasher := md5.New()
 		io.WriteString(hasher, fmt.Sprintf("%s%d", input, counter))
 		hash := fmt.Sprintf("%x", hasher.Sum(nil))
 		if strings.HasPrefix(hash, "00000") {
-			fmt.Printf("%s", hash[5:6])
-			found++
+			if foundPart1 < 8 {
+				fmt.Printf("%s", hash[5:6])
+				foundPart1++
+			}
+
+			if foundPart2 < 8 {
+				pos, err := strconv.Atoi(hash[5:6])
+				if err == nil && pos < 8 && passwordPart2[pos] == "" {
+					passwordPart2[pos] = hash[6:7]
+					foundPart2++
+				}
+			}
 		}
 		counter++
 	}
+	fmt.Println()
+	fmt.Println(strings.Join(passwordPart2, ""))
 }
